e6a635a010192a20ac1428a48184a6c84cd9f3dc
electrica <electricaev@gmail.com>
2019-11-09 20:06:33 +0600

    ClientConfig

---
+++ _build/elements/tv.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'show-main' => [
+        'type' => 'textfield',
+        'caption' => 'Показывать на главной',
+        'description' => 'Описание ТВ',
+        'category' => 'Главная',
+        'resources' => []
+    ]
+];
